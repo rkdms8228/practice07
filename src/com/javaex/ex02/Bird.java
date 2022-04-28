379313918d53165ba4a235b6cbd2151879066560
가은 <rkdms8228@naver.com>
2022-04-28 16:16:28 +0900

    package02

--- src/com/javaex/ex02/Bird.java
+++ src/com/javaex/ex02/Bird.java
@@ -1,8 +1,32 @@
 package com.javaex.ex02;
 
 public abstract class Bird {
-    private String name;
+	
+	//필드
+	protected String name;
+	
+	//생성자
+	public Bird() {
+	}
+	
+	public Bird(String name) {
+		this.name = name;
+	}
+	
+	//메소드-gs
+	public String getName() {
+		return name;
+	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	//메소드-일반
+	public abstract void fly(); 
 
-
+	public abstract void sing(); 
+	
+	public abstract void showName();
+	
 }
--- src/com/javaex/ex02/Duck.java
+++ src/com/javaex/ex02/Duck.java
@@ -2,16 +2,28 @@ package com.javaex.ex02;
 
 public class Duck extends Bird {
 
-    public void sing() {
-    
-    }
+	//필드
+	
+	//생성자
+	
+	//메소드-gs
+	
+	//메소드-일반
+	public void fly() {
+		System.out.println("오리("+super.getName()+")가 날지 않습니다.");
+	}
+	
+	public void sing() {
+		System.out.println("오리("+super.getName()+")가 소리내어 웁니다.");
+	}
 
-    public void fly() {
-    
-    }
-    
-    public void showName() {
-    
-    }
+    @Override
+	public String toString() {
+		return "Duck [getName()=" + getName() + "]";
+	}
 
+	public void showName() {
+    	System.out.println("오리의 이름은 "+super.getName()+" 입니다.");
+    }
+    
 }
